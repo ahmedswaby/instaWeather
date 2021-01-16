2fe327b0bdcadc75b024ae2acddf2513da68fb79
ahmedswaby <ahmed.khaledswaby@gmail.com>
2021-01-16 10:52:27 +0200

    responsivness & active temp / day

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -6,10 +6,69 @@ class Main extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            temp: 'f',
+            view: 'h'
         };
     }
 
     componentDidMount() {
+        navigator.geolocation.getCurrentPosition(this.showPosition)
+    }
+    //     function getWeatherAPI(apiLinkDS) {
+    //     fetch(apiLinkDS, {
+    //         method: 'GET',
+    //     }).then(function (response) {
+    //         return response.json()
+    //     }).then(function (data) {
+    //         temp.innerHTML = Math.round(data.currently.temperature);
+    //         cond.innerHTML = data.currently.summary;
+    //         weatherIcon(data.currently.icon);
+    //     });
+    // }
+
+    showPosition = (position) => {
+        console.log(position.coords.latitude);
+        console.log(position.coords.longitude);
+        this.fetchData(position)
+    }
+    async fetchData(position) {
+        const weatherUrl = `https://api.darksky.net/forecast/a177f8481c31fa96c3f95ad4f4f84610/${position.coords.latitude},${position.coords.longitude}/`
+        await fetch(weatherUrl, {
+            'Access-Control-Allow-Origin': '*',
+            method: 'GET',
+            credentials: 'include',
+            headers: new Headers()
+        })
+            .then((resp) => {
+                return resp
+            })
+            .then((data) => {
+                console.log(data);
+                const result = data.json()
+                console.log(result);
+            }).catch(err => {
+                console.log(err);
+            })
+
+    }
+    toggleTemp = (val) => {
+        if (val) {
+
+            this.setState({
+                temp: val
+            })
+        }
+    }
+    toggleView = (val) => {
+
+        this.setState({
+            view: val
+        })
+
+        // if (val === "F") {
+        //     return Math.round(val * (9 / 5) + 32);
+        // }
+        // return Math.round((val - 32) * (5 / 9));
     }
     render() {
         return (
@@ -19,11 +78,11 @@ class Main extends React.Component {
                         <h4 className="text-uppercase">
                             instaweather
                         </h4>
-                        <div className="d-flex">
-                            <button className="btn text-white">
+                        <div className="d-flex ml-auto">
+                            <button className={`btn text-white ${this.state.temp === 'c' ? 'active' : ''}`} onClick={() => this.toggleTemp('c')}>
                                 C
                             </button>
-                            <button className="btn active text-white">
+                            <button className={`btn text-white ${this.state.temp === 'f' ? 'active' : ''}`} onClick={(e) => this.toggleTemp('f')}>
                                 F
                             </button>
                         </div>
@@ -49,93 +108,129 @@ class Main extends React.Component {
                             </div>
                         </div>
                         <div className="border-bottom mb-4">
-                            <button className="btn active pl-0 pr-0">
+                            <button className={`btn pl-0 pr-0 ${this.state.view === 'h' ? 'active' : ''}`}
+                                onClick={() => this.toggleView('h')}>
                                 hourly
                             </button>
-                            <button className="btn">
+                            <button className={`btn ${this.state.view === 'd' ? 'active' : ''}`} onClick={() => this.toggleView('d')}>
                                 daily
                             </button>
                         </div>
-                        <div className="d-flex overflow-scroll pb-4">
-                            <div className="hourly-section">
-                                <p className="text-capitalize">now</p>
-                                <img src={sunny} alt="sunny day" />
-                                <h4>81<sup>°</sup></h4>
+                        {this.state.view === 'h' ?
+                            <div className="d-flex overflow-scroll pb-4">
+                                <div className="hourly-section">
+                                    <p className="text-capitalize">now</p>
+                                    <img src={sunny} alt="sunny day" />
+                                    <h4>81<sup>°</sup></h4>
+                                </div>
+                                <div className="hourly-section">
+                                    <p className="text-capitalize">now</p>
+                                    <img src={sunny} alt="sunny day" />
+                                    <h4>81<sup>°</sup></h4>
+                                </div>
+                                <div className="hourly-section">
+                                    <p className="text-capitalize">now</p>
+                                    <img src={sunny} alt="sunny day" />
+                                    <h4>81<sup>°</sup></h4>
+                                </div>
+                                <div className="hourly-section">
+                                    <p className="text-capitalize">now</p>
+                                    <img src={sunny} alt="sunny day" />
+                                    <h4>81<sup>°</sup></h4>
+                                </div>
+                                <div className="hourly-section">
+                                    <p className="text-capitalize">now</p>
+                                    <img src={sunny} alt="sunny day" />
+                                    <h4>81<sup>°</sup></h4>
+                                </div>
+                                <div className="hourly-section">
+                                    <p className="text-capitalize">now</p>
+                                    <img src={sunny} alt="sunny day" />
+                                    <h4>81<sup>°</sup></h4>
+                                </div>
+                                <div className="hourly-section">
+                                    <p className="text-capitalize">now</p>
+                                    <img src={sunny} alt="sunny day" />
+                                    <h4>81<sup>°</sup></h4>
+                                </div>
+                                <div className="hourly-section">
+                                    <p className="text-capitalize">now</p>
+                                    <img src={sunny} alt="sunny day" />
+                                    <h4>81<sup>°</sup></h4>
+                                </div>
+                                <div className="hourly-section">
+                                    <p className="text-capitalize">now</p>
+                                    <img src={sunny} alt="sunny day" />
+                                    <h4>81<sup>°</sup></h4>
+                                </div>
+                                <div className="hourly-section">
+                                    <p className="text-capitalize">now</p>
+                                    <img src={sunny} alt="sunny day" />
+                                    <h4>81<sup>°</sup></h4>
+                                </div>
+                                <div className="hourly-section">
+                                    <p className="text-capitalize">now</p>
+                                    <img src={sunny} alt="sunny day" />
+                                    <h4>81<sup>°</sup></h4>
+                                </div>
+                                <div className="hourly-section">
+                                    <p className="text-capitalize">now</p>
+                                    <img src={sunny} alt="sunny day" />
+                                    <h4>81<sup>°</sup></h4>
+                                </div>
+                                <div className="hourly-section">
+                                    <p className="text-capitalize">now</p>
+                                    <img src={sunny} alt="sunny day" />
+                                    <h4>81<sup>°</sup></h4>
+                                </div>
+                                <div className="hourly-section">
+                                    <p className="text-capitalize">now</p>
+                                    <img src={sunny} alt="sunny day" />
+                                    <h4>81<sup>°</sup></h4>
+                                </div>
+                                <div className="hourly-section">
+                                    <p>now</p>
+                                    <img src={cloudy} alt="cloudy day" />
+                                    <h4>81<sup>°</sup></h4>
+                                </div>
                             </div>
-                            <div className="hourly-section">
-                                <p className="text-capitalize">now</p>
-                                <img src={sunny} alt="sunny day" />
-                                <h4>81<sup>°</sup></h4>
+                            :
+                            <div className="d-flex overflow-scroll pb-4">
+                                <div className="hourly-section">
+                                    <p className="text-capitalize">Today</p>
+                                    <img src={sunny} alt="sunny day" />
+                                    <h4>81<sup>°</sup></h4>
+                                </div>
+                                <div className="hourly-section">
+                                    <p className="text-capitalize">now</p>
+                                    <img src={sunny} alt="sunny day" />
+                                    <h4>81<sup>°</sup></h4>
+                                </div>
+                                <div className="hourly-section">
+                                    <p className="text-capitalize">now</p>
+                                    <img src={sunny} alt="sunny day" />
+                                    <h4>81<sup>°</sup></h4>
+                                </div>
+                                <div className="hourly-section">
+                                    <p className="text-capitalize">now</p>
+                                    <img src={sunny} alt="sunny day" />
+                                    <h4>81<sup>°</sup></h4>
+                                </div>
+                                <div className="hourly-section">
+                                    <p className="text-capitalize">now</p>
+                                    <img src={sunny} alt="sunny day" />
+                                    <h4>81<sup>°</sup></h4>
+                                </div>
+                                <div className="hourly-section">
+                                    <p className="text-capitalize">now</p>
+                                    <img src={sunny} alt="sunny day" />
+                                    <h4>81<sup>°</sup></h4>
+                                </div>
                             </div>
-                            <div className="hourly-section">
-                                <p className="text-capitalize">now</p>
-                                <img src={sunny} alt="sunny day" />
-                                <h4>81<sup>°</sup></h4>
-                            </div>
-                            <div className="hourly-section">
-                                <p className="text-capitalize">now</p>
-                                <img src={sunny} alt="sunny day" />
-                                <h4>81<sup>°</sup></h4>
-                            </div>
-                            <div className="hourly-section">
-                                <p className="text-capitalize">now</p>
-                                <img src={sunny} alt="sunny day" />
-                                <h4>81<sup>°</sup></h4>
-                            </div>
-                            <div className="hourly-section">
-                                <p className="text-capitalize">now</p>
-                                <img src={sunny} alt="sunny day" />
-                                <h4>81<sup>°</sup></h4>
-                            </div>
-                            <div className="hourly-section">
-                                <p className="text-capitalize">now</p>
-                                <img src={sunny} alt="sunny day" />
-                                <h4>81<sup>°</sup></h4>
-                            </div>
-                            <div className="hourly-section">
-                                <p className="text-capitalize">now</p>
-                                <img src={sunny} alt="sunny day" />
-                                <h4>81<sup>°</sup></h4>
-                            </div>
-                            <div className="hourly-section">
-                                <p className="text-capitalize">now</p>
-                                <img src={sunny} alt="sunny day" />
-                                <h4>81<sup>°</sup></h4>
-                            </div>
-                            <div className="hourly-section">
-                                <p className="text-capitalize">now</p>
-                                <img src={sunny} alt="sunny day" />
-                                <h4>81<sup>°</sup></h4>
-                            </div>
-                            <div className="hourly-section">
-                                <p className="text-capitalize">now</p>
-                                <img src={sunny} alt="sunny day" />
-                                <h4>81<sup>°</sup></h4>
-                            </div>
-                            <div className="hourly-section">
-                                <p className="text-capitalize">now</p>
-                                <img src={sunny} alt="sunny day" />
-                                <h4>81<sup>°</sup></h4>
-                            </div>
-                            <div className="hourly-section">
-                                <p className="text-capitalize">now</p>
-                                <img src={sunny} alt="sunny day" />
-                                <h4>81<sup>°</sup></h4>
-                            </div>
-                            <div className="hourly-section">
-                                <p className="text-capitalize">now</p>
-                                <img src={sunny} alt="sunny day" />
-                                <h4>81<sup>°</sup></h4>
-                            </div>
-                            <div className="hourly-section">
-                                <p>now</p>
-                                <img src={cloudy} alt="cloudy day" />
-                                <h4>81<sup>°</sup></h4>
-                            </div>
-                        </div>
+                        }
                     </main>
                 </div>
-            </section>
+            </section >
         )
     }
 }
