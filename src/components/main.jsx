44c9211b0118d846768591d9ca0e9b0ab63d0014
ahmedswaby <ahmed.khaledswaby@gmail.com>
2021-02-28 17:02:41 +0200

    Revert "create node server"
    This reverts commit ca5071def4dc988848290e1d8165cde3b6249b34.

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -14,7 +14,6 @@ class Main extends React.Component {
     }
 
     componentDidMount() {
-        console.log(process.env.PORT)
         // if (navigator.geolocation) {
         // document.querySelector(".loader-wrapper").classList.remove('d-none')
         navigator.geolocation.getCurrentPosition(this.showPosition,
@@ -40,7 +39,7 @@ class Main extends React.Component {
     // fetch data deponds on the long & lat
     async fetchData(position) {
         // https://cors-anywhere.herokuapp.com/
-        const weatherUrl = `${process.env.PORT ? process.env.PORT / 'testAPI/' : 'https://thingproxy.freeboard.io/fetch/'}https://api.darksky.net/forecast/a177f8481c31fa96c3f95ad4f4f84610/${position.coords.latitude},${position.coords.longitude}/`
+        const weatherUrl = `https://thingproxy.freeboard.io/fetch/https://api.darksky.net/forecast/a177f8481c31fa96c3f95ad4f4f84610/${position.coords.latitude},${position.coords.longitude}/`
         await fetch(weatherUrl, {
             method: 'GET',
             headers: {
